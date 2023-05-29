a8e33b29193ceb2697d173c8f5881c9bdd78ed87
David Wilson de Jesus <davidwj15@gmail.com>
2023-05-28 22:35:23 -0300

    Ajuste nos botões

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import HeaderVue from "./views/Header.vue";
 import SearchVue from "./views/Search.vue";
+import { ref } from 'vue';
 
+const buttons = ref([
+  { id: 1, label: 'favorite', isActive: false },
+  { id: 2, label: 'emAlta', isActive: true },
+  { id: 3, label: 'novas', isActive: false },
+]);
+
+const toggleButton = (buttonId: number) => {
+  buttons.value.forEach((button) => {
+    button.isActive = button.id === buttonId
+  })
+}
 
 </script>
 <script lang="ts">
@@ -23,17 +35,20 @@ export default {
       <div class="flex justify-center items-center mb-5">
         <ul class="flex">
           <li class="me-3">
-            <button href="#" class="bg-purple-800 border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+            <button href="#"
+              class="bg-purple-800 border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950">
               Cotação
             </button>
           </li>
           <li class="me-3">
-            <button href="#" class="border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+            <button href="#"
+              class="border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950">
               Simulador
             </button>
           </li>
           <li class="">
-            <button href="#" class="border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+            <button href="#"
+              class="border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950">
               Comparação
             </button>
           </li>
@@ -52,20 +67,26 @@ export default {
           </li>
           <li class="me-3">
             <router-link to="/favorite">
-              <button class="border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+              <button :key="buttons[0].id" @click="toggleButton(buttons[0].id)"
+                class="border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950"
+                :class="{ 'bg-purple-800': buttons[0].isActive }">
                 <FontAwesomeIcon :icon="['fas', 'star']" />
               </button>
             </router-link>
           </li>
           <li class="me-3">
             <router-link to="/">
-              <button class="bg-purple-800 border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+              <button :key="buttons[1].id" @click="toggleButton(buttons[1].id)"
+                class=" border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950"
+                :class="{ 'bg-purple-800': buttons[1].isActive }">
                 Em alta
               </button>
             </router-link>
           </li>
           <li class="">
-            <button href="#" class="border-2 border-purple-800 sm:p-2 md:p-3 rounded-full font-bold">
+            <button :key="buttons[2].id" @click="toggleButton(buttons[2].id)"
+              class="border-2 border-purple-950 sm:p-2 md:p-3 rounded-full font-bold hover:shadow-md hover:shadow-purple-950"
+              :class="{ 'bg-purple-800': buttons[2].isActive }">
               Novas
             </button>
           </li>
